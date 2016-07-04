ddf8f3be4ee624f9df232bda992cc4833ddf7f7a
duan89 <duanzhongyi1989@icloud.com>
2016-07-04 15:59:05 +0900

    ceshi

---
+++ src/cn/test/array1/Test01.java
@@ -0,0 +1,20 @@
+package cn.test.array1;
+
+import org.junit.Test;
+
+public class Test01 {
+
+	@Test
+	public void testArray() {
+		StringBuilder ab = new StringBuilder("abcdefghijklmnopqrsts");
+		StringBuilder ab = new StringBuilder("abcdefghijklmnopqrsts");
+		StringBuilder a = new StringBuilder("abcdefghijklmnopqrsts");
+		ab.delete(2, 5);
+		System.out.println(ab);
+	}
+
+
+	public Test01() {
+	}
+
+}
